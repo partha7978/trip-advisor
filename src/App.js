0de05520279c2ce472e18529900d932b1484220c
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-02-09 23:12:03 +0530

    adding folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Map from './Components/Map/Map';
+import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+
 
 const App = () => {
     return (
         <div>
             <h1>Trip advisor</h1>
+            <Header />
+            <List />
+            <Map />
+            <PlaceDetails />
         </div>
     );
 }
