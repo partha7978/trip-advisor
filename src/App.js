029ce2e6dcc0357d5fd2bb5b7a7ef8ce97e41b31
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-02-10 23:22:19 +0530

    adding layout grid to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import Header from './Components/Header/Header';
-import List from './Components/List/List';
-import Map from './Components/Map/Map';
-import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+import React from "react";
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
 
-
+import { CssBaseline, Grid } from "@mui/material";
 const App = () => {
     return (
-        <div>
-            <h1>Trip advisor</h1>
+        <>
+            <CssBaseline />
             <Header />
-            <List />
-            <Map />
-            <PlaceDetails />
-        </div>
+            <Grid container spacing={3} style={{ width: "100%" }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     );
-}
+};
 
 export default App;
