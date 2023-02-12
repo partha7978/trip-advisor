4dce6e1e79e2f7fee0b805a9b11d86d544468f1b
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-02-12 22:06:29 +0530

    installing mui icons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,27 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
 export default function Header() {
     return (
         <AppBar position="static">
-           
+            <Toolbar>
+                <Typography variant="h5" >
+                    Trip Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" >
+                        Explore new places
+                    </Typography>
+                </Box>
+                {/* <AutoComplete> */}
+                    <div>
+                        <div>
+                            {/* <SearchIcon /> */}SR icon
+                        </div>
+                        <InputBase
+                            placeholder="Search..."
+                      
+                        />
+                    </div>
+                {/* </AutoComplete> */}
+            </Toolbar>
         </AppBar>
     );
 }
