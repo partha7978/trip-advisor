6748242e6eddb6f3e3d7c7e18f30815bb7d7b206
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-02-14 21:50:31 +0530

    changing the structure of navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 // import AutoComplete from "@react/google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 // import useStyles from "./styles";
 
 export default function Header() {
     return (
         <AppBar position="static">
-            <Toolbar >
-                <Typography variant="h5" >
-                    Trip Advisor
-                </Typography>
-                <Box display="flex">
-                    <Typography variant="h6" >
-                        Explore new places
-                    </Typography>
-                </Box>
-                {/* <AutoComplete> */}
-                    <div >
-                        <div >
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search..."
-                      
-                        />
+            <Toolbar>
+                <div className="navBar">
+                    <div className="logo">
+
                     </div>
-                {/* </AutoComplete> */}
+                    <div className="searchBar">
+                        <Typography variant="h5">Explore New Places</Typography>
+                        <input type="text" className="searchBox" placeholder="search here ... " />
+                    </div>
+                </div>
             </Toolbar>
         </AppBar>
     );
